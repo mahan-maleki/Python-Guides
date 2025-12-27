bc9b50ce82c73ecd85129e105f4ce0843e535256
Mahan Maleki <mahanmaleki950@gmail.com>
2025-12-27 16:19:40 +0330

    Create Data Types python file

---
+++ data-types.py
@@ -0,0 +1,23 @@
+# String
+string_data = "Hello World"
+
+# Int
+integer_data = 1
+
+# Float
+float_data = 3.14
+
+# List
+list_data = [1,2,3,4,5]
+
+# Dictionary
+dictionary_data = {"a":1, "b":2, "c":3, "d":4}
+
+# Tuple
+tuple_data = (1,2,3,4,5)
+
+# Set
+set_data = {1, 2, 3, 4, 5}
+
+# Boolean
+bool_data = True
