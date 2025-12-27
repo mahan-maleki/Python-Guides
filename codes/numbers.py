ad81cef16efc4af9ebe13e588e2a3fb870b3c0f3
Mahan Maleki <mahanmaleki950@gmail.com>
2025-12-27 16:39:51 +0330

    Create Number Data Types python file

---
+++ codes/numbers.py
@@ -0,0 +1,26 @@
+# integer number
+int_number = 10         #for example: 35, 94, 265, -4566, etc.
+
+# float number
+float_number = 3.14     #for example: 65.3, 0.3, 24.0, 2.0000002, -0,12
+
+# plural
+plural = 2 + 5
+
+# subtraction
+subtraction = 22 - 11
+
+# multiplication
+multiplication = 11 * 2
+
+# division
+division = 18 / 6
+
+# integer division
+int_division = 18 // 6
+
+# power
+power = 2 ** 5          #it means: 2^5
+
+# mod
+mod = 21 % 5            #it means: Remainder of division
