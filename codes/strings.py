3e474d2a6b109bc4e0f01f5868434f6404a0268d
Mahan Maleki <mahanmaleki950@gmail.com>
2025-12-28 22:59:34 +0330

    Create strings.py file

---
+++ codes/strings.py
@@ -0,0 +1,10 @@
+string_data = "Hello, I'm Mahan Maleki !"
+
+string_index = string_data[4]
+
+string_slice = string_data[:5]
+string_slice2 = string_data[5:]
+string_slice3 = string_data[::2]
+string_slice4 = string_data[7::2]
+
+print(string_data + "   ", string_index + "   ", string_slice + "   ", string_slice2 + "   ", string_slice3 + "   ", string_slice4 + "   ")
