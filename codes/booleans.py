4bf4e5aa188d2f1ae4308de908c14b7cd4f9754c
Mahan Maleki <mahanmaleki950@gmail.com>
2025-12-30 21:21:53 +0330

    Create booleans.py files

---
+++ codes/booleans.py
@@ -0,0 +1,23 @@
+bool_one = True
+bool_two = False
+
+print(bool_one)
+print(bool_two)
+
+print(2 > 1)
+print(2 < 3)
+print(13 <= 12)
+print(14 >= 13)
+print(45 == 45)
+print(44 != 45)
+
+
+type(None)
+
+
+print(3 > 4 or 3 == 3)
+print(3 < 4 and 3 <= 10)
+print(3 > 4 and not 4)
+
+myList = [1,2,3,4,5]
+print(3 in myList)
