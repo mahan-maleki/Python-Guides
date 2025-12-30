49354afc14c2ef9c38b3b7bfca36f8120732a378
Mahan Maleki <mahanmaleki950@gmail.com>
2025-12-30 10:23:23 +0330

    Create sets.py file

---
+++ codes/sets.py
@@ -0,0 +1,18 @@
+chess_class = set()
+
+chess_class.add('ali')
+chess_class.add('hassan')
+chess_class.add('mahan')
+print(chess_class)
+
+sofal_class = set()
+
+sofal_class.add('mamad')
+sofal_class.add('hassan')
+print(sofal_class)
+
+print(sofal_class.union(chess_class))
+print(sofal_class.intersection(chess_class))
+
+heyvoons = set(["sag", "gorbe", "fok", "khar"])
+print(heyvoons)
