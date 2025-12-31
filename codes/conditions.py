fbd55b61662d81b44e30f86964d0c3c76fc0537b
Mahan Maleki <mahanmaleki950@gmail.com>
2025-12-31 21:39:49 +0330

    Create conditions.py file

---
+++ codes/conditions.py
@@ -0,0 +1,14 @@
+sen = 45
+
+if sen < 10:
+    print("Sen is less than 10")
+elif sen < 30:
+    print("Sen is less than 30")
+elif sen < 50:
+    print("Sen is less than 50")
+elif sen < 100:
+    print("Sen is less than 100")
+elif sen < 200:
+    print("hanooz namordi ?")
+else:
+    print("bemir digeh")
