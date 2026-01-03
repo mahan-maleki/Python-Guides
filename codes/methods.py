bf0ffa853fdc7156ca3091d3c53afc9f9074f435
Mahan Maleki <mahanmaleki950@gmail.com>
2026-01-03 14:17:50 +0330

    Create methods.py file

---
+++ codes/methods.py
@@ -0,0 +1,15 @@
+l = [1, 2, 3]
+
+last_index = l.pop()
+
+print(last_index)
+print(l)
+
+
+name = 'mahan'
+
+uppercase_name = name.upper()
+lowercase_name = name.lower()
+
+print(uppercase_name)
+print(lowercase_name)
