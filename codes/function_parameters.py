26fdeb66e24d2894a14b6ec6978f81ab9f0ea51e
Mahan Maleki <mahanmaleki950@gmail.com>
2026-01-04 02:46:22 +0330

    Create function_parameters.py file

---
+++ codes/function_parameters.py
@@ -0,0 +1,14 @@
+def sum_two(num1, num2):
+    return num1 + num2
+
+num1 = 5
+num2 = 2
+
+res = sum_two(num1, num2)
+print(res)
+
+def print_times(name, n=1):
+    for _ in range(n):
+        print(name)
+
+print_times('jadi')
